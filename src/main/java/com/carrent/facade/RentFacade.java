776e8bc1f2b672b299cd27c9a7bc49ba5e884534
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2022-10-09 17:24:03 +0200

    createRent method updated

--- src/main/java/com/carrent/facade/RentFacade.java
+++ src/main/java/com/carrent/facade/RentFacade.java
@@ -1,10 +1,13 @@
 package com.carrent.facade;
 
+import com.carrent.core.Car;
 import com.carrent.core.Rent;
 import com.carrent.core.RentDto;
 import com.carrent.mappers.RentMapper;
 import com.carrent.repositories.RentRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -13,12 +16,24 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class RentFacade {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RentFacade.class);
+    private static final String FREE = "Free";
+    private static final String RENTED = "Rented";
     private final RentRepository rentRepository;
     private final RentMapper rentMapper;
 
     public Long createRent(RentDto rentDto) {
         Rent rent = rentMapper.mapToRent(rentDto);
-        rentRepository.save(rent);
+        Car car = rent.getCarId();
+
+        if(car.getRentStatus() == FREE) {
+            car.setRentStatus(RENTED);
+            rentRepository.save(rent);
+
+        }else {
+            LOGGER.error("CAR IS ALREADY RENTED");
+        }
+
         return rent.getId();
     }
 
