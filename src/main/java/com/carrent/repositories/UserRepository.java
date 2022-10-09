70b3e1f31b76173700221e6c93d5dd8cf020d005
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2022-10-09 23:14:45 +0200

    findById method added

--- src/main/java/com/carrent/repositories/UserRepository.java
+++ src/main/java/com/carrent/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findById(long id);
 }
